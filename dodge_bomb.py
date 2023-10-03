86ba068fbabbbcb4391fedbd45c8e67918dae17b
rayohno <c0b2202940@edu.teu.ac.jp>
2023-10-03 14:56:43 +0900

    練習３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -6,14 +6,25 @@ import pygame as pg
 WIDTH, HEIGHT = 1600, 900
 
 
+delta = {
+    pg.K_UP:(0,-5),
+    pg.K_DOWN:(0,+5),
+    pg.K_LEFT:(-5,0),
+    pg.K_RIGHT:(+5,0),
+}
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+    """こうかとん"""
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    clock = pg.time.Clock()
-    tmr = 0
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = (900,400)
+    
+    """ばくだん"""
     bomb = pg.Surface((20,20))
     pg.draw.circle(bomb, (255, 0, 0), (10, 10), 10)
     bomb.set_colorkey((0, 0, 0))
@@ -22,20 +33,35 @@ def main():
     bd_rct.center = x,y
     vx,vy = +5, +5
     
+    clock = pg.time.Clock()
+    tmr = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-
+        
+            
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        # screen.blit(kk_img, [900, 400])
+        
+        """こうかとん"""
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0,0]
+        for key, mv in delta.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv[0],sum_mv[1])
+        screen.blit(kk_img,kk_rct)
+        
+        """ばくだん"""
         bd_rct.move_ip(vx,vy)
         screen.blit(bomb,bd_rct)
+        
         pg.display.update()
         tmr += 1
         clock.tick(50)
-        
-        """bomb"""
 
 
 if __name__ == "__main__":
