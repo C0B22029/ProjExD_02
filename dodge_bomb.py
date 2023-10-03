fe9d21d75e0154f3a83d71857e6b8003d645c430
rayohno <c0b2202940@edu.teu.ac.jp>
2023-10-03 14:19:00 +0900

    練習一

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import pygame as pg
 
@@ -13,6 +14,13 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
     tmr = 0
+    bomb = pg.Surface((20,20))
+    pg.draw.circle(bomb, (255, 0, 0), (10, 10), 10)
+    bomb.set_colorkey((0, 0, 0))
+    bd_rct = bomb.get_rect()
+    x,y = random.randint(0,WIDTH),random.randint(0,HEIGHT)
+    bd_rct.center = x,y
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -20,6 +28,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bomb,bd_rct)
         pg.display.update()
         tmr += 1
         clock.tick(10)
